671f0557e2ee9c55857ebf068d8208ba65f81c77
Alex Sova <foxeyes.ml@gmail.com>
2021-06-18 09:28:36 +0300

    chore: css buld stage removed, auto re-move

--- render/render.js
+++ render/render.js
@@ -1,13 +1,3 @@
 export {ReHtm} from './re-htm.js';
 export {ReScript} from './re-script.js';
 export {ReMd} from './re-md.js';
-
-window.addEventListener('keyup', (e) => {
-  if (e.altKey && e.key === 's') {
-    e.preventDefault();
-    [...document.querySelectorAll('[to-remove]')].forEach((el) => {
-      el.remove();
-    });
-    let html
-  }
-})
